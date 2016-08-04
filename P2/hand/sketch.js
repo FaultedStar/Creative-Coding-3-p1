fcb9cfea1621c226b2b6decd4466776f2ddbc2f4
FaultedStar <zellerphoe@e-01-03-lab.mddn.vuw.ac.nz>
2016-08-04 17:30:57 +1200

    made hand sketch progress

--- P2/hand/sketch.js
+++ P2/hand/sketch.js
@@ -16,71 +16,87 @@ var canvasHeight = 500;
 var savedValues = {
   "A":
     {
-      "box1": {
-        "position": -200,
-        "tilt": 90
-      },
-      "box2": {
-        "position": -154,
-        "tilt": 93
-      },
-      "box3": {
-        "position": -114,
-        "tilt": 93
-      },
-      "box4": {
-        "position": -124,
-        "tilt": 66
-      },
-      "box5": {
-        "position": -93,
-        "tilt": 96
-      }
-
-    },
-  "B":
-    {
   "box1": {
-    "position": -199,
-    "tilt": 90
+    "x": 0,
+    "y": 65,
+    "tilt": -58
   },
   "box2": {
-    "position": -157,
+    "x": 0,
+    "y": -26,
     "tilt": 93
   },
   "box3": {
-    "position": -114,
+    "x": 0,
+    "y": 5,
+    "tilt": 90
+  },
+  "box4": {
+    "x": 0,
+    "y": 37,
+    "tilt": 90
+  },
+  "box5": {
+    "x": 0,
+    "y": 72,
+    "tilt": 90
+  }
+}
+
+    ,
+  "B":
+    {
+  "box1": {
+    "x": -199,
+    "y": -200,
+    "tilt": 90
+  },
+  "box2": {
+    "x": -157,
+    "y": -200,
+    "tilt": 93
+  },
+  "box3": {
+    "x": -114,
+    "y": -200,
     "tilt": 93
   },
   "box4": {
-    "position": -13,
+    "x": -13,
+    "y": -200,
     "tilt": 51
   },
   "box5": {
-    "position": -93,
+    "x": -93,
+    "y": -200,
     "tilt": 96
   }
 },
   "C":
     {
   "box1": {
-    "position": 154,
+    "x": 154,
+    "y": -200,
     "tilt": -9
   },
   "box2": {
-    "position": 118,
+    "x": 118,
+    "y": -200,
     "tilt": 1
   },
   "box3": {
-    "position": -200,
+    "x": -200,
+    "y": -200,
     "tilt": 114
   },
   "box4": {
-    "position": -13,
+    "x": -13,
+    "y": -200,
     "tilt": 26
   },
   "box5": {
-    "position": -93,
+    "x": -93,
+    "y": -200,
     "tilt": 91
   }
 }
@@ -95,14 +111,19 @@ function setup () {
 
   // create two sliders
   pos1x_slider = createSlider(-200, 200, 0);
+  pos1y_slider = createSlider(-200, 200, 0);
   tilt1_slider = createSlider(-180, 180, 0);
   pos2x_slider = createSlider(-200, 200, 0);
+  pos2y_slider = createSlider(-200, 200, 0);
   tilt2_slider = createSlider(-180, 180, 0);
   pos3x_slider = createSlider(-200, 200, 0);
+  pos3y_slider = createSlider(-200, 200, 0);
   tilt3_slider = createSlider(-180, 180, 0);
   pos4x_slider = createSlider(-200, 200, 0);
+  pos4y_slider = createSlider(-200, 200, 0);
   tilt4_slider = createSlider(-180, 180, 0);
   pos5x_slider = createSlider(-200, 200, 0);
+  pos5y_slider = createSlider(-200, 200, 0);
   tilt5_slider = createSlider(-180, 180, 0);
   sel = createSelect();
   sel.option('A');
@@ -113,18 +134,23 @@ function setup () {
   button = createButton('show data');
   button.mousePressed(buttonPressedEvent);
 
-  // position each element on the page
+  // x each element on the page
   main_canvas.parent('canvasContainer');
-  pos1x_slider.parent('slider1Container');
-  tilt1_slider.parent('slider2Container');
-  pos2x_slider.parent('slider3Container');
-  tilt2_slider.parent('slider4Container');
-  pos3x_slider.parent('slider5Container');
-  tilt3_slider.parent('slider6Container');
-   pos4x_slider.parent('slider7Container');
-   tilt4_slider.parent('slider8Container');
-	pos5x_slider.parent('slider9Container');
-  	tilt5_slider.parent('slider10Container');
+  pos1x_slider.parent('slider1xContainer');
+  pos1y_slider.parent('slider1yContainer');
+  tilt1_slider.parent('slider1tContainer');
+  pos2x_slider.parent('slider2xContainer');
+  pos2y_slider.parent('slider2yContainer');
+  tilt2_slider.parent('slider2tContainer');
+  pos3x_slider.parent('slider3xContainer');
+  pos3y_slider.parent('slider3yContainer');
+  tilt3_slider.parent('slider3tContainer');
+  pos4x_slider.parent('slider4xContainer');
+  pos4y_slider.parent('slider4yContainer');
+  tilt4_slider.parent('slider4tContainer');
+	pos5x_slider.parent('slider5xContainer');
+  pos5y_slider.parent('slider5yContainer');
+  tilt5_slider.parent('slider5tContainer');
 
 
   sel.parent(selectorContainer);
@@ -134,33 +160,43 @@ function setup () {
 function sliderToDataObject() {
   var obj = {};
   obj["box1"] = {};
-  obj["box1"]["position"] = pos1_slider.value();
+  obj["box1"]["x"] = pos1x_slider.value();
+  obj["box1"]["y"] = pos1y_slider.value();
   obj["box1"]["tilt"] = tilt1_slider.value();
   obj["box2"] = {};
-  obj["box2"]["position"] = pos2_slider.value();
+  obj["box2"]["x"] = pos2x_slider.value();
+  obj["box2"]["y"] = pos2y_slider.value();
   obj["box2"]["tilt"] = tilt2_slider.value();
   obj["box3"] = {};
-  obj["box3"]["position"] = pos3_slider.value();
+  obj["box3"]["x"] = pos3x_slider.value();
+  obj["box3"]["y"] = pos3y_slider.value();
   obj["box3"]["tilt"] = tilt3_slider.value();
-    obj["box4"] = {};
-  obj["box4"]["position"] = pos4_slider.value();
+   obj["box4"] = {};
+  obj["box4"]["x"] = pos4x_slider.value();
+  obj["box4"]["y"] = pos4y_slider.value();
   obj["box4"]["tilt"] = tilt4_slider.value();
-    obj["box5"] = {};
-  obj["box5"]["position"] = pos5_slider.value();
+  obj["box5"] = {};
+  obj["box5"]["x"] = pos5x_slider.value();
+  obj["box5"]["y"] = pos5y_slider.value();
   obj["box5"]["tilt"] = tilt5_slider.value();
   return obj;
 }
 
 function dataObjectToSliders(obj) {
-  pos1_slider.value(obj["box1"]["position"]);
+  pos1x_slider.value(obj["box1"]["x"]);
+  pos1y_slider.value(obj["box1"]["y"]);
   tilt1_slider.value(obj["box1"]["tilt"]);
-  pos2_slider.value(obj["box2"]["position"]);
+  pos2x_slider.value(obj["box2"]["x"]);
+  pos2y_slider.value(obj["box2"]["y"]);
   tilt2_slider.value(obj["box2"]["tilt"]);
-  pos3_slider.value(obj["box3"]["position"]);
+  pos3x_slider.value(obj["box3"]["x"]);
+  pos3y_slider.value(obj["box3"]["y"]);
   tilt3_slider.value(obj["box3"]["tilt"]);
-    pos4_slider.value(obj["box4"]["position"]);
+  pos4x_slider.value(obj["box4"]["x"]);
+  pos4y_slider.value(obj["box4"]["y"]);
   tilt4_slider.value(obj["box4"]["tilt"]);
-    pos5_slider.value(obj["box5"]["position"]);
+  pos5x_slider.value(obj["box5"]["x"]);
+  pos5y_slider.value(obj["box5"]["y"]);
   tilt5_slider.value(obj["box5"]["tilt"]);
 }
 
@@ -179,26 +215,27 @@ var colorFront = [207, 222, 227];
 var colorBack = '#ffffcc';
 
 ////////////////////////////////////////////////////DRAWING BIT LOOK
-function drawPart(y_offset, pos, tilt) {
-  var middle_x = 2 * canvasWidth / 3;
-  var middle_y = canvasHeight / 4*3;
+function drawFinger(x, y, tilt, colour, scale) {
+  var middle_x = canvasWidth / 3*2;
+  var middle_y = canvasHeight / 2;
   resetMatrix();
-  translate(middle_x + pos, middle_y + y_offset);
+  translate(middle_x, middle_y);
   rotate(tilt);
 
-  var scale = 10;
+  //var scale = 10;
 
-  fill(colorFront);
+  fill(colour);
   // rect(-100,-100,100,100);
-  //ellipse(-20*scale, -3*scale, 20*scale, 3*scale);
-  ellipse(0,0,30,60)
+  ellipse(x, y, 20*scale, 3*scale);
+  //ellipse(0,0,30,60)
 }
 
-function drawFromSliders(y_offset, pos_slider, tilt_slider) {
-  var pos, tilt;
-  pos = pos_slider.value();
+function drawFromSliders(x_offset, y_offset, tilt_slider,colour, scale) {
+  var x, y, tilt;
+  x = x_offset.value();
+  y = y_offset.value();
   tilt = tilt_slider.value();
-  drawPart(y_offset, pos, tilt);
+  drawFinger(x,y, tilt, colour, scale);
 }
 
 function draw () {
@@ -206,11 +243,11 @@ function draw () {
   fill(colorFront);
   stroke(95, 52, 8);
 //////////////////////////////// put x sliders here
-  drawFromSliders(-50, pos1x_slider, tilt1_slider);
-  drawFromSliders(  0, pos2x_slider, tilt2_slider);
-  drawFromSliders( 50, pos3x_slider, tilt3_slider);
-    drawFromSliders( 100, pos4x_slider, tilt4_slider);
-      drawFromSliders( 150, pos5x_slider, tilt5_slider);
+  drawFromSliders(pos1x_slider, pos1y_slider, tilt1_slider,'#666699', 12);
+  drawFromSliders(pos2x_slider, pos2y_slider, tilt2_slider, '#4ce600',10);
+  drawFromSliders(pos3x_slider, pos3y_slider, tilt3_slider, '#ffff33',10);
+    drawFromSliders(pos4x_slider, pos4y_slider, tilt4_slider,'#ffa31a',10);
+      drawFromSliders(pos5x_slider, pos5y_slider, tilt5_slider, '#ff3300',8);
 }
 
 function keyTyped() {
