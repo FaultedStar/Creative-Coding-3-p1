7c568d46f470c067c085e07a06fb0e0d260622ae
FaultedStar <phoebe.zeller@hotmail.com>
2016-08-12 16:35:47 +1200

    Fingers up and running
    woo

--- P2/working on fingers/README.md
+++ P2/working on fingers/README.md
@@ -1,6 +1,7 @@
 ## PS2 MDDN 342 2016
 Phoebe Zeller 
 300309856
-Made some really good behind the scenes progression today. Fingers are now colour coded and are drawn with a bunch of new veriables and this is also updated in the JSON file.
-still a long way to come. 
-This is the image im working off to create the hand shapes https://upload.wikimedia.org/wikipedia/en/6/6f/Aslfingerspellalpha.png
+This is maybe the most ugly code I've ever written. That said, the fingers now have joints and each have sliders. 
+This means that I can get some hand shapes that accually looks like hands. There are still a few things I need to touch up.
+The fingers can't straighten properly So I need to edit the sliders. The order in which the fingers are wrong so I need to write a function for that. 
+Finger nails also should be drawn in order to know the orientation of the fingers.
--- P2/working on fingers/sketch.js
+++ P2/working on fingers/sketch.js
@@ -17,101 +17,190 @@ var savedValues = {
   "A":
 {
   "box1": {
-    "x": -25,
-    "y": 65,
-    "tilt": -58
+    "x1": 0,
+    "y1": 73,
+    "tilt1": -91,
+    "x2": -68,
+    "y2": -68,
+    "tilt2": 89,
+    "x3": 104,
+    "y3": 65,
+    "tilt3": -89
   },
   "box2": {
-    "x": 0,
-    "y": -26,
-    "tilt": 93
+    "x1": -104,
+    "y1": -32,
+    "tilt1": 91,
+    "x2": -43,
+    "y2": -38,
+    "tilt2": 89,
+    "x3": 12,
+    "y3": -38,
+    "tilt3": 91
   },
   "box3": {
-    "x": 0,
-    "y": 5,
-    "tilt": 90
+    "x1": -104,
+    "y1": -2,
+    "tilt1": 91,
+    "x2": -43,
+    "y2": -4,
+    "tilt2": 89,
+    "x3": 12,
+    "y3": -4,
+    "tilt3": 91
   },
   "box4": {
-    "x": 0,
-    "y": 37,
-    "tilt": 90
+    "x1": -104,
+    "y1": 25,
+    "tilt1": 91,
+    "x2": -43,
+    "y2": 22,
+    "tilt2": 89,
+    "x3": 12,
+    "y3": 22,
+    "tilt3": 91
   },
   "box5": {
-    "x": 5,
-    "y": 66,
-    "tilt": 90  
+    "x1": -92,
+    "y1": 50,
+    "tilt1": 91,
+    "x2": -57,
+    "y2": 50,
+    "tilt2": 89,
+    "x3": -14,
+    "y3": 50,
+    "tilt3": 90
   },
   "palm": {
-     "x": 0,
-      "y": 0,
-    "width": 200
+    "x": 0,
+    "y": 0,
+    "width": 184
   }
 },
   "B":
+ {
+  "box1": {
+    "x1": 13,
+    "y1": -68,
+    "tilt1": 90,
+    "x2": 0,
+    "y2": -31,
+    "tilt2": 27,
+    "x3": -78,
+    "y3": -31,
+    "tilt3": 30
+  },
+  "box2": {
+    "x1": -103,
+    "y1": -32,
+    "tilt1": 91,
+    "x2": -160,
+    "y2": -38,
+    "tilt2": 89,
+    "x3": -200,
+    "y3": -38,
+    "tilt3": 91
+  },
+  "box3": {
+    "x1": -104,
+    "y1": -2,
+    "tilt1": 91,
+    "x2": -153,
+    "y2": -4,
+    "tilt2": 89,
+    "x3": -200,
+    "y3": -4,
+    "tilt3": 91
+  },
+  "box4": {
+    "x1": -104,
+    "y1": 25,
+    "tilt1": 91,
+    "x2": -167,
+    "y2": 22,
+    "tilt2": 89,
+    "x3": -200,
+    "y3": 22,
+    "tilt3": 91
+  },
+  "box5": {
+    "x1": -92,
+    "y1": 50,
+    "tilt1": 91,
+    "x2": -124,
+    "y2": 47,
+    "tilt2": 89,
+    "x3": -160,
+    "y3": 50,
+    "tilt3": 90
+  },
+  "palm": {
+    "x": 0,
+    "y": 0,
+    "width": 184
+  }
+},
+  "C":
    {
   "box1": {
-    "x": -11,
-    "y": 8,
-    "tilt": 45
+    "x1": -71,
+    "y1": 68,
+    "tilt1": -6,
+    "x2": -14,
+    "y2": 68,
+    "tilt2": -16,
+    "x3": 43,
+    "y3": 68,
+    "tilt3": -25
   },
   "box2": {
-    "x": -120,
-    "y": -26,
-    "tilt": 93
+    "x1": -60,
+    "y1": -92,
+    "tilt1": 0,
+    "x2": -3,
+    "y2": -106,
+    "tilt2": 0,
+    "x3": 29,
+    "y3": -96,
+    "tilt3": 16
   },
   "box3": {
-    "x": -125,
-    "y": 5,
-    "tilt": 90
+    "x1": -64,
+    "y1": -92,
+    "tilt1": 0,
+    "x2": -7,
+    "y2": -106,
+    "tilt2": 0,
+    "x3": 29,
+    "y3": -96,
+    "tilt3": 16
   },
   "box4": {
-    "x": -117,
-    "y": 37,
-    "tilt": 90
+    "x1": -67,
+    "y1": -92,
+    "tilt1": 0,
+    "x2": -10,
+    "y2": -106,
+    "tilt2": 0,
+    "x3": 25,
+    "y3": -96,
+    "tilt3": 16
   },
   "box5": {
-    "x": -100,
-    "y": 66,
-    "tilt": 82
+    "x1": -67,
+    "y1": -92,
+    "tilt1": 0,
+    "x2": -28,
+    "y2": -96,
+    "tilt2": 0,
+    "x3": 0,
+    "y3": -92,
+    "tilt3": 7
   },
   "palm": {
-    "x": -26,
-    "y": 19,
-    "width": 207
-  }
-}
-,
-  "C":
-    {
-  "box1": {
-    "x": 4,
-    "y": 59,
-    "tilt": -13
-  },
-  "box2": {
-    "x": 0,
-    "y": -81,
-    "tilt": -3
-  },
-  "box3": {
-    "x": 14,
-    "y": -75,
-    "tilt": 0
-  },
-  "box4": {
-    "x": 0,
-    "y": -64,
-    "tilt": 0
-  },
-  "box5": {
-    "x": -11,
-    "y": -59,
-    "tilt": 0
-  },
-  "palm": {
-    "x": -94,
-    "y": -4,
-    "width": 71
+    "x": -86,
+    "y": -3,
+    "width": 81
   }
 }
 }
@@ -123,25 +212,82 @@ function setup () {
   // rotation in degrees (more slider friendly)
   angleMode(DEGREES);
 
-  // create sliders                              
-  pos1x_slider = createSlider(-200, 200, 0);
-  pos1y_slider = createSlider(-200, 200, 0);
-  tilt1_slider = createSlider(-180, 180, 0);
-  pos2x_slider = createSlider(-200, 200, 0);
-  pos2y_slider = createSlider(-200, 200, 0);
-  tilt2_slider = createSlider(-180, 180, 0);
-  pos3x_slider = createSlider(-200, 200, 0);
-  pos3y_slider = createSlider(-200, 200, 0);
-  tilt3_slider = createSlider(-180, 180, 0);
-  pos4x_slider = createSlider(-200, 200, 0);
-  pos4y_slider = createSlider(-200, 200, 0);
-  tilt4_slider = createSlider(-180, 180, 0);
-  pos5x_slider = createSlider(-200, 200, 0);
-  pos5y_slider = createSlider(-200, 200, 0);
-  tilt5_slider = createSlider(-180, 180, 0);
+  // create sliders                   
+  // finger 1 AKA Thumb           
+  pos1x1_slider = createSlider(-200, 200, 0); // closest
+  pos1y1_slider = createSlider(-200, 200, 0);
+  tilt11_slider = createSlider(-180, 180, 0);
+
+
+  pos1x2_slider = createSlider(-200, 200, 0); //Middle
+  pos1y2_slider = createSlider(-200, 200, 0);
+  tilt12_slider = createSlider(-180, 180, 0);
+
+
+  pos1x3_slider = createSlider(-200, 200, 0);//tip
+  pos1y3_slider = createSlider(-200, 200, 0);
+  tilt13_slider = createSlider(-180, 180, 0);
+
+
+
+// Finger 2 AKA Pointer
+  pos2x1_slider = createSlider(-200, 200, 0); // closest
+  pos2y1_slider = createSlider(-200, 200, 0);
+  tilt21_slider = createSlider(-180, 180, 0);
+
+  pos2x2_slider = createSlider(-200, 200, 0);//Middle
+  pos2y2_slider = createSlider(-200, 200, 0);
+  tilt22_slider = createSlider(-180, 180, 0);
+
+  pos2x3_slider = createSlider(-200, 200, 0);//tip
+  pos2y3_slider = createSlider(-200, 200, 0);
+  tilt23_slider = createSlider(-180, 180, 0);
+
+  // Finger 3 AKA Middle Finger
+  pos3x1_slider = createSlider(-200, 200, 0);  // closest
+  pos3y1_slider = createSlider(-200, 200, 0);
+  tilt31_slider = createSlider(-180, 180, 0);
+
+  pos3x2_slider = createSlider(-200, 200, 0);//Middle
+  pos3y2_slider = createSlider(-200, 200, 0);
+  tilt32_slider = createSlider(-180, 180, 0);
+  
+  pos3x3_slider = createSlider(-200, 200, 0);//tip
+  pos3y3_slider = createSlider(-200, 200, 0);
+  tilt33_slider = createSlider(-180, 180, 0);
+
+  // finger 4 AKA Ring finger
+  pos4x1_slider = createSlider(-200, 200, 0);  // closest
+  pos4y1_slider = createSlider(-200, 200, 0);
+  tilt41_slider = createSlider(-180, 180, 0);
+
+  pos4x2_slider = createSlider(-200, 200, 0);//Middle
+  pos4y2_slider = createSlider(-200, 200, 0);
+  tilt42_slider = createSlider(-180, 180, 0);
+
+  pos4x3_slider = createSlider(-200, 200, 0);//tip
+  pos4y3_slider = createSlider(-200, 200, 0);
+  tilt43_slider = createSlider(-180, 180, 0);
+
+  // finger 5 AKA Pinky
+  pos5x1_slider = createSlider(-200, 200, 0); // closest
+  pos5y1_slider = createSlider(-200, 200, 0);
+  tilt51_slider = createSlider(-180, 180, 0);
+
+  pos5x2_slider = createSlider(-200, 200, 0);//Middle
+  pos5y2_slider = createSlider(-200, 200, 0);
+  tilt52_slider = createSlider(-180, 180, 0);
+
+
+  pos5x3_slider = createSlider(-200, 200, 0);//tip
+  pos5y3_slider = createSlider(-200, 200, 0);
+  tilt53_slider = createSlider(-180, 180, 0);
+
+
   palmX_slider = createSlider(-180, 180, 0);
   palmY_slider = createSlider(-180, 180, 0);
   palmWidth_slider = createSlider(10, 250, 100);
+
   sel = createSelect();
   sel.option('A');
   sel.option('B');
@@ -152,22 +298,77 @@ function setup () {
   button.mousePressed(buttonPressedEvent);
 
   // x each element on the page
-  main_canvas.parent('canvasContainer');       
-  pos1x_slider.parent('slider1xContainer');
-  pos1y_slider.parent('slider1yContainer');
-  tilt1_slider.parent('slider1tContainer');
-  pos2x_slider.parent('slider2xContainer');
-  pos2y_slider.parent('slider2yContainer');
-  tilt2_slider.parent('slider2tContainer');
-  pos3x_slider.parent('slider3xContainer');
-  pos3y_slider.parent('slider3yContainer');
-  tilt3_slider.parent('slider3tContainer');
-  pos4x_slider.parent('slider4xContainer');
-  pos4y_slider.parent('slider4yContainer');
-  tilt4_slider.parent('slider4tContainer');
-	pos5x_slider.parent('slider5xContainer');
-  pos5y_slider.parent('slider5yContainer');
-  tilt5_slider.parent('slider5tContainer');
+  main_canvas.parent('canvasContainer');      
+
+  //finger 1
+  pos1x1_slider.parent('slider1x1Container');
+  pos1y1_slider.parent('slider1y1Container');
+  tilt11_slider.parent('slider1t1Container');
+
+
+  pos1x2_slider.parent('slider1x2Container');
+  pos1y2_slider.parent('slider1y2Container');
+  tilt12_slider.parent('slider1t2Container');
+
+  pos1x3_slider.parent('slider1x3Container'); //point
+  pos1y3_slider.parent('slider1y3Container');
+  tilt13_slider.parent('slider1t3Container');
+
+  //finger 2
+  pos2x1_slider.parent('slider2x1Container');
+  pos2y1_slider.parent('slider2y1Container');
+  tilt21_slider.parent('slider2t1Container');
+
+  pos2x2_slider.parent('slider2x2Container');
+  pos2y2_slider.parent('slider2y2Container');
+  tilt22_slider.parent('slider2t2Container');
+
+  pos2x3_slider.parent('slider2x3Container'); //point
+  pos2y3_slider.parent('slider2y3Container');
+  tilt23_slider.parent('slider2t3Container');
+
+	//finger 3
+  pos3x1_slider.parent('slider3x1Container');
+  pos3y1_slider.parent('slider3y1Container');
+  tilt31_slider.parent('slider3t1Container');
+
+  pos3x2_slider.parent('slider3x2Container');
+  pos3y2_slider.parent('slider3y2Container');
+  tilt32_slider.parent('slider3t2Container');
+
+  pos3x3_slider.parent('slider3x3Container'); //point
+  pos3y3_slider.parent('slider3y3Container');
+  tilt33_slider.parent('slider3t3Container');
+
+  //finger 4
+  pos4x1_slider.parent('slider4x1Container');
+  pos4y1_slider.parent('slider4y1Container');
+  tilt41_slider.parent('slider4t1Container');
+
+  pos4x2_slider.parent('slider4x2Container');
+  pos4y2_slider.parent('slider4y2Container');
+  tilt42_slider.parent('slider4t2Container');
+
+  pos4x3_slider.parent('slider4x3Container'); //point
+  pos4y3_slider.parent('slider4y3Container');
+  tilt43_slider.parent('slider4t3Container');
+
+
+  //finger 5
+  pos5x1_slider.parent('slider5x1Container');
+  pos5y1_slider.parent('slider5y1Container');
+  tilt51_slider.parent('slider5t1Container');
+
+  pos5x2_slider.parent('slider5x2Container');
+  pos5y2_slider.parent('slider5y2Container');
+  tilt52_slider.parent('slider5t2Container');
+
+  pos5x3_slider.parent('slider5x3Container'); //point
+  pos5y3_slider.parent('slider5y3Container');
+  tilt53_slider.parent('slider5t3Container');
+
+
+
   palmX_slider.parent('palmXContainer');
   palmY_slider.parent('palmYContainer');
    palmWidth_slider.parent('palmWidthContainer'); 
@@ -179,25 +380,86 @@ function setup () {
 function sliderToDataObject() {       
   var obj = {};
   obj["box1"] = {};
-  obj["box1"]["x"] = pos1x_slider.value();
-  obj["box1"]["y"] = pos1y_slider.value();
-  obj["box1"]["tilt"] = tilt1_slider.value();
+  obj["box1"]["x1"] = pos1x1_slider.value();
+  obj["box1"]["y1"] = pos1y1_slider.value();
+  obj["box1"]["tilt1"] = tilt11_slider.value();
+
+
+  obj["box1"]["x2"] = pos1x2_slider.value();
+  obj["box1"]["y2"] = pos1y2_slider.value();
+  obj["box1"]["tilt2"] = tilt12_slider.value();
+
+  
+  obj["box1"]["x3"] = pos1x3_slider.value();
+  obj["box1"]["y3"] = pos1y3_slider.value();
+  obj["box1"]["tilt3"] = tilt13_slider.value();
+
+
+
   obj["box2"] = {};
-  obj["box2"]["x"] = pos2x_slider.value();
-  obj["box2"]["y"] = pos2y_slider.value();
-  obj["box2"]["tilt"] = tilt2_slider.value();
+  obj["box2"]["x1"] = pos2x1_slider.value();
+  obj["box2"]["y1"] = pos2y1_slider.value();
+  obj["box2"]["tilt1"] = tilt21_slider.value();
+
+
+
+  obj["box2"]["x2"] = pos2x2_slider.value();
+  obj["box2"]["y2"] = pos2y2_slider.value();
+  obj["box2"]["tilt2"] = tilt22_slider.value();
+
+
+  obj["box2"]["x3"] = pos2x3_slider.value();
+  obj["box2"]["y3"] = pos2y3_slider.value();
+  obj["box2"]["tilt3"] = tilt23_slider.value();
+
+
   obj["box3"] = {};
-  obj["box3"]["x"] = pos3x_slider.value();
-  obj["box3"]["y"] = pos3y_slider.value();
-  obj["box3"]["tilt"] = tilt3_slider.value();
+  obj["box3"]["x1"] = pos3x1_slider.value();
+  obj["box3"]["y1"] = pos3y1_slider.value();
+  obj["box3"]["tilt1"] = tilt31_slider.value();
+
+
+  obj["box3"]["x2"] = pos3x2_slider.value();
+  obj["box3"]["y2"] = pos3y2_slider.value();
+  obj["box3"]["tilt2"] = tilt32_slider.value();
+
+  obj["box3"]["x3"] = pos3x3_slider.value();
+  obj["box3"]["y3"] = pos3y3_slider.value();
+  obj["box3"]["tilt3"] = tilt33_slider.value();
+
+
    obj["box4"] = {};
-  obj["box4"]["x"] = pos4x_slider.value();
-  obj["box4"]["y"] = pos4y_slider.value();
-  obj["box4"]["tilt"] = tilt4_slider.value();
+  obj["box4"]["x1"] = pos4x1_slider.value();
+  obj["box4"]["y1"] = pos4y1_slider.value();
+  obj["box4"]["tilt1"] = tilt41_slider.value();
+
+
+  obj["box4"]["x2"] = pos4x2_slider.value();
+  obj["box4"]["y2"] = pos4y2_slider.value();
+  obj["box4"]["tilt2"] = tilt42_slider.value();
+
+
+  obj["box4"]["x3"] = pos4x3_slider.value();
+  obj["box4"]["y3"] = pos4y3_slider.value();
+  obj["box4"]["tilt3"] = tilt43_slider.value();
+
+
   obj["box5"] = {};
-  obj["box5"]["x"] = pos5x_slider.value();
-  obj["box5"]["y"] = pos5y_slider.value();
-  obj["box5"]["tilt"] = tilt5_slider.value();
+  obj["box5"]["x1"] = pos5x1_slider.value();
+  obj["box5"]["y1"] = pos5y1_slider.value();
+  obj["box5"]["tilt1"] = tilt51_slider.value();
+
+
+  obj["box5"]["x2"] = pos5x2_slider.value();
+  obj["box5"]["y2"] = pos5y2_slider.value();
+  obj["box5"]["tilt2"] = tilt52_slider.value();
+
+
+  obj["box5"]["x3"] = pos5x3_slider.value();
+  obj["box5"]["y3"] = pos5y3_slider.value();
+  obj["box5"]["tilt3"] = tilt53_slider.value();
+
+
    obj["palm"] = {};
    obj["palm"]["x"] = palmX_slider.value();
    obj["palm"]["y"] = palmY_slider.value();
@@ -206,21 +468,82 @@ function sliderToDataObject() {
 }
 
 function dataObjectToSliders(obj) {     
-  pos1x_slider.value(obj["box1"]["x"]);
-  pos1y_slider.value(obj["box1"]["y"]);
-  tilt1_slider.value(obj["box1"]["tilt"]);
-  pos2x_slider.value(obj["box2"]["x"]);
-  pos2y_slider.value(obj["box2"]["y"]);
-  tilt2_slider.value(obj["box2"]["tilt"]);
-  pos3x_slider.value(obj["box3"]["x"]);
-  pos3y_slider.value(obj["box3"]["y"]);
-  tilt3_slider.value(obj["box3"]["tilt"]);
-  pos4x_slider.value(obj["box4"]["x"]);
-  pos4y_slider.value(obj["box4"]["y"]);
-  tilt4_slider.value(obj["box4"]["tilt"]);
-  pos5x_slider.value(obj["box5"]["x"]);
-  pos5y_slider.value(obj["box5"]["y"]);
-  tilt5_slider.value(obj["box5"]["tilt"]);
+	//ONE
+  pos1x1_slider.value(obj["box1"]["x1"]);
+  pos1y1_slider.value(obj["box1"]["y1"]);
+  tilt11_slider.value(obj["box1"]["tilt1"]);
+
+   pos1x2_slider.value(obj["box1"]["x2"]);
+  pos1y2_slider.value(obj["box1"]["y2"]);
+  tilt12_slider.value(obj["box1"]["tilt2"]);
+
+   pos1x3_slider.value(obj["box1"]["x3"]);
+  pos1y3_slider.value(obj["box1"]["y3"]);
+  tilt13_slider.value(obj["box1"]["tilt3"]);
+
+//TWO
+  pos2x1_slider.value(obj["box2"]["x1"]);
+  pos2y1_slider.value(obj["box2"]["y1"]);
+  tilt21_slider.value(obj["box2"]["tilt1"]);
+
+
+  pos2x2_slider.value(obj["box2"]["x2"]);
+  pos2y2_slider.value(obj["box2"]["y2"]);
+  tilt22_slider.value(obj["box2"]["tilt2"]);
+
+
+  pos2x3_slider.value(obj["box2"]["x3"]);
+  pos2y3_slider.value(obj["box2"]["y3"]);
+  tilt23_slider.value(obj["box2"]["tilt3"]);
+
+
+//THREE
+  pos3x1_slider.value(obj["box3"]["x1"]);
+  pos3y1_slider.value(obj["box3"]["y1"]);
+  tilt31_slider.value(obj["box3"]["tilt1"]);
+
+
+  pos3x2_slider.value(obj["box3"]["x2"]);
+  pos3y2_slider.value(obj["box3"]["y2"]);
+  tilt32_slider.value(obj["box3"]["tilt2"]);
+  
+  pos3x3_slider.value(obj["box3"]["x3"]);
+  pos3y3_slider.value(obj["box3"]["y3"]);
+  tilt33_slider.value(obj["box3"]["tilt3"]);
+  
+
+
+//FOUR
+  pos4x1_slider.value(obj["box4"]["x1"]);
+  pos4y1_slider.value(obj["box4"]["y1"]);
+  tilt41_slider.value(obj["box4"]["tilt1"]);
+
+
+  pos4x2_slider.value(obj["box4"]["x2"]);
+  pos4y2_slider.value(obj["box4"]["y2"]);
+  tilt42_slider.value(obj["box4"]["tilt2"]);
+
+
+  pos4x3_slider.value(obj["box4"]["x3"]);
+  pos4y3_slider.value(obj["box4"]["y3"]);
+  tilt43_slider.value(obj["box4"]["tilt3"]);
+
+//FIVE
+  pos5x1_slider.value(obj["box5"]["x1"]);
+  pos5y1_slider.value(obj["box5"]["y1"]);
+  tilt51_slider.value(obj["box5"]["tilt1"]);
+
+
+  pos5x2_slider.value(obj["box5"]["x2"]);
+  pos5y2_slider.value(obj["box5"]["y2"]);
+  tilt52_slider.value(obj["box5"]["tilt2"]);
+
+
+  pos5x3_slider.value(obj["box5"]["x3"]);
+  pos5y3_slider.value(obj["box5"]["y3"]);
+  tilt53_slider.value(obj["box5"]["tilt3"]);
+
+
   palmX_slider.value(obj["palm"]["x"]);
   palmY_slider.value(obj["palm"]["y"]);
   palmWidth_slider.value(obj["palm"]["width"]);
@@ -253,7 +576,7 @@ function drawFinger(x, y, tilt, colour, scale) {
 
   fill(colour);
   // rect(-100,-100,100,100);
-  ellipse(x, y, 20*scale, 3*scale);
+  ellipse(x, y, 20*scale, 10*scale);
   pop();
   //ellipse(0,0,30,60)
 }
@@ -284,11 +607,32 @@ function draw () {
   fill(colorFront);
   stroke(95, 52, 8);
   drawPalm(palmX_slider, palmY_slider, palmWidth_slider);
-  drawFromSliders(pos1x_slider, pos1y_slider, tilt1_slider,'#666699', 12);
-  drawFromSliders(pos2x_slider, pos2y_slider, tilt2_slider, '#4ce600',10);
-  drawFromSliders(pos3x_slider, pos3y_slider, tilt3_slider, '#ffff33',10);
-  drawFromSliders(pos4x_slider, pos4y_slider, tilt4_slider,'#ffa31a',10);
-  drawFromSliders(pos5x_slider, pos5y_slider, tilt5_slider, '#ff3300',8);
+
+   //one
+  drawFromSliders(pos1x1_slider, pos1y1_slider, tilt11_slider,'#666699', 5); // purple
+  drawFromSliders(pos1x2_slider, pos1y2_slider, tilt12_slider,'#666699', 5);
+  drawFromSliders(pos1x3_slider, pos1y3_slider, tilt13_slider,'#666699', 5);
+  
+  //two
+  drawFromSliders(pos2x1_slider, pos2y1_slider, tilt21_slider, '#4ce600',3); // Green
+  drawFromSliders(pos2x2_slider, pos2y2_slider, tilt22_slider, '#4ce600',3);
+  drawFromSliders(pos2x3_slider, pos2y3_slider, tilt23_slider, '#4ce600',3);
+  
+  //three
+  drawFromSliders(pos3x1_slider, pos3y1_slider, tilt31_slider, '#ffff33',3); // yellow
+  drawFromSliders(pos3x2_slider, pos3y2_slider, tilt32_slider, '#ffff33',3);
+  drawFromSliders(pos3x3_slider, pos3y3_slider, tilt33_slider, '#ffff33',3);
+  
+
+  //four
+  drawFromSliders(pos4x1_slider, pos4y1_slider, tilt41_slider,'#ffa31a',3); // orange
+  drawFromSliders(pos4x2_slider, pos4y2_slider, tilt42_slider,'#ffa31a',3);
+  drawFromSliders(pos4x3_slider, pos4y3_slider, tilt43_slider,'#ffa31a',3);
+
+  //five
+  drawFromSliders(pos5x1_slider, pos5y1_slider, tilt51_slider, '#ff3300',2); //red 
+  drawFromSliders(pos5x2_slider, pos5y2_slider, tilt52_slider, '#ff3300',2);
+  drawFromSliders(pos5x3_slider, pos5y3_slider, tilt53_slider, '#ff3300',2);
   
 
 }
